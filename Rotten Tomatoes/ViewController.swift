cc7cca3c11a71dfe013ff4212dc171f9e8eaca8c
Dang Quoc Huy <huysamdua@yahoo.com>
2016-06-01 11:44:33 +0700

    Initial Commit

---
+++ Rotten Tomatoes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rotten Tomatoes
+//
+//  Created by Dang Quoc Huy on 6/1/16.
+//  Copyright © 2016 Dang Quoc Huy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
