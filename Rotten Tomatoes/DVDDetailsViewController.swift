a116667dc1bf8255697c4b99d4e2a4e9cd9100b9
Dang Quoc Huy <huysamdua@yahoo.com>
2016-06-09 08:48:24 +0700

    finish dvd detail view

--- Rotten Tomatoes/DVDDetailsViewController.swift
+++ Rotten Tomatoes/DVDDetailsViewController.swift
@@ -10,17 +10,24 @@ import UIKit
 
 class DVDDetailsViewController: UIViewController {
 
+    // MARK: - Properties
+    
     @IBOutlet weak var posterImage: UIImageView!
     @IBOutlet weak var swipeView: UIView!
     @IBOutlet weak var swipeButton: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var swipeView_HeightConstrant: NSLayoutConstraint!
     
     var DVD: NSDictionary!
     
+    // MARK: - Functions
+    
+    // Called after the controller's view is loaded into memory
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // config swipe up/down gesture for swipeView
         let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(DVDDetailsViewController.onDetailViewSwipe))
         swipeUp.direction = .Up
         let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(DVDDetailsViewController.onDetailViewSwipe))
@@ -29,9 +36,11 @@ class DVDDetailsViewController: UIViewController {
         swipeButton.addGestureRecognizer(swipeDown)
         swipeButton.userInteractionEnabled = true
         
+        // retreive DVD title and synopsis info
         titleLabel.text = DVD["title"] as? String
         synopsisLabel.text = DVD["synopsis"] as? String
         
+        // retreive DVD poster (load low resolution first, full size later)
         let thumbnailLink = DVD.valueForKeyPath("posters.thumbnail") as! String
         let imageLink = self.DVD.valueForKeyPath("posters.detailed") as! String
         self.posterImage.setImageWithThumbnail(thumbnailLink, imageLink: imageLink)
@@ -41,26 +50,26 @@ class DVDDetailsViewController: UIViewController {
         self.tabBarController?.tabBar.hidden = true
     }
 
+    // Sent to the view controller when the app receives a memory warning.
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    // triggered on event swipe up/down swipeView
     func onDetailViewSwipe(sender: UIGestureRecognizer) {
         if let swipeGesture = sender as? UISwipeGestureRecognizer {
             switch swipeGesture.direction {
             case UISwipeGestureRecognizerDirection.Up:
-                let frame = self.posterImage.frame
+                let height = self.posterImage.frame.height
                 UIView.animateWithDuration(0.5, animations: {
-                    self.swipeView.frame = frame
+                    self.swipeView_HeightConstrant.constant = height
+                    self.view.layoutIfNeeded()
                 })
             case UISwipeGestureRecognizerDirection.Down:
-                let frame = self.swipeView.frame
                 UIView.animateWithDuration(0.5, animations: {
-                    self.swipeView.frame = CGRect(  x: frame.origin.x,
-                                                            y: (frame.height - 150 + frame.origin.y),
-                                                            width: frame.width,
-                                                            height: 150)
+                    self.swipeView_HeightConstrant.constant = 150
+                    self.view.layoutIfNeeded()
                 })
             default:
                 break
